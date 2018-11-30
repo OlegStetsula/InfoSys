d850395dfa44ac9960ab8cccb44bdf52df331ca0
OlegStetsula <olegstetsula@gmail.com>
2018-11-30 20:01:06 +0200

    Add project files.

---
+++ InfoSys/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InfoSys.MvcApplication" Language="C#" %>
---
+++ InfoSys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfoSys.Startup))]
+namespace InfoSys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
